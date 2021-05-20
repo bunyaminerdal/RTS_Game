c314a6dd8aa89a624b0e6b2e792f88e168429975
leonidas131 <leonidas131@hotmail.com>
2021-05-20 20:59:07 +0300

    Revert "inventory continue"
    This reverts commit 1d9d9923e194802b17079a08ffd08235ac8e9294.

--- Assets/Scripts/ScriptableObjects/Inventory/Scripts/UserInterface.cs
+++ Assets/Scripts/ScriptableObjects/Inventory/Scripts/UserInterface.cs
@@ -16,9 +16,6 @@ public abstract class UserInterface : MonoBehaviour
     public Dictionary<GameObject, InventorySlot> slotsOnInterface = new Dictionary<GameObject, InventorySlot>();
     public Dictionary<GameObject, Attribute> textOnInterface = new Dictionary<GameObject, Attribute>();
     private PlayerInputActions UIActions;
-    private bool isDisplayed;
-    public bool IsDisplayed { get => isDisplayed; set => isDisplayed = value; }
-
     private void Awake()
     {
         UIActions = new PlayerInputActions();
@@ -35,6 +32,9 @@ public abstract class UserInterface : MonoBehaviour
     {
         unitInventory = new UnitInventory(6);
         unitEqInventory = new UnitInventory(3);
+        unitInventory = null;
+        unitEqInventory = null;
+        unit = null;
         AddEvent(gameObject, EventTriggerType.PointerEnter, delegate { OnEnterInterface(gameObject); });
         AddEvent(gameObject, EventTriggerType.PointerExit, delegate { OnExitInterface(gameObject); });
 
@@ -44,7 +44,7 @@ public abstract class UserInterface : MonoBehaviour
     void Update()
     {
 
-        if (isDisplayed)
+        if (unitInventory != null)
         {
             slotsOnInterface.UpdateSlotDisplay();
             //textOnInterface.UpdateTextDisplay();
@@ -53,10 +53,19 @@ public abstract class UserInterface : MonoBehaviour
     }
     public void SetInventory(UnitInventory inventory)
     {
+        unitInventory = null;
+        slotsOnInterface.Clear();
+        if (transform.childCount > 0)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+        }
         unitInventory = inventory;
         CreateSlots();
-        slotsOnInterface.UpdateSlotDisplay();
     }
+
 
 
     public abstract void CreateSlots();
